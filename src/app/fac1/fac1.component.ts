cb6eeeaafc17c1d0341d97257afb6f4f428d3640
arjunbaladarshan <60723978+arjunbaladarshan@users.noreply.github.com>
2025-01-01 09:21:38 +0530

    SubRoute Added

--- src/app/fac1/fac1.component.ts
+++ src/app/fac1/fac1.component.ts
@@ -1,11 +1,14 @@
+import { JsonPipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-fac1',
-  imports: [],
+  imports: [FormsModule, JsonPipe],
   templateUrl: './fac1.component.html',
   styleUrl: './fac1.component.css'
 })
 export class Fac1Component {
-
+  name = "arjun"
+  isRadioChecked = false;
 }
